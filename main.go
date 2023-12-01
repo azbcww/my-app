cc2b1ab005f7271b25f49d53eac46850bb3ac0a7
azbcww <1121hatsuyuki@gmail.com>
2023-12-02 04:24:13 +0900

    session function

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
-	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
-	"github.com/traPtitech/naro-template-backend/handler"
 	"log"
 	"os"
 	"time"
+
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/srinathgs/mysqlstore"
+	"github.com/traPtitech/naro-template-backend/handler"
+
+	"github.com/go-sql-driver/mysql"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -40,18 +46,31 @@ func main() {
 		log.Fatal(err)
 	}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (Username VARCHAR(255) PRIMARY KEY, HashedPass VARCHAR(255))") 
-	if err != nil { 
-		log.Fatal(err) 
-	} 
+	// usersテーブルが存在しなかったら、usersテーブルを作成する
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (Username VARCHAR(255) PRIMARY KEY, HashedPass VARCHAR(255))")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// セッションの情報を記憶するための場所をデータベース上に設定
+	store, err := mysqlstore.NewMySQLStoreFromConnection(db.DB, "sessions", "/", 60*60*24*14, []byte("secret-token"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	h := handler.NewHandler(db)
 	e := echo.New()
+	e.Use(middleware.Logger())       // ログを取るミドルウェアを追加
+	e.Use(session.Middleware(store)) // セッション管理のためのミドルウェアを追加
 
-	e.POST("/signup", h.SignUpHandler) 
+	e.POST("/signup", h.SignUpHandler)
+	e.POST("/login", h.LoginHandler)
 
-	e.GET("/cities/:cityName", h.GetCityInfoHandler)
-	e.POST("/cities", h.PostCityHandler)
+	withAuth := e.Group("")
+	withAuth.Use(handler.UserAuthMiddleware)
+	withAuth.GET("/me", handler.GetMeHandler)
+	withAuth.GET("/cities/:cityName", h.GetCityInfoHandler)
+	withAuth.POST("/cities", h.PostCityHandler)
 
 	err = e.Start(":8080")
 	if err != nil {
